8a683944031c0e08e21fc474751f3760c8390b03
mkdir_overthir <39881724+jakegoss@users.noreply.github.com>
2021-08-30 12:05:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,14 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC61b5962310dbcd5cd22542043d9e657e"
+account_sid = "xxxxxxxxxxxxxxxxxxxxx"
 # Your Auth Token from twilio.com/console
-auth_token  = "b8ce06b367327840287e7f8c35b27c16"
+auth_token  = "yyyyyyyyyyyyyyyyyyyyy"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+18024983534", 
+    to="+12345874587", 
     from_="+13363447244",
     body="Hello from Python!")
 
